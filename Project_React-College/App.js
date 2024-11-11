76066c0515dea66f59cd5c887a5c8852e0d14d8c
Vivianne Christi <61090532+VivianneChristi@users.noreply.github.com>
2024-11-11 16:29:45 -0300

    Criada a página para o aluno e a página para o professor com layouts e navegação ajustados.
    - Tela de Aluno: Adicionada saudação personalizada, foto de perfil, lista de aulas e banner de biblioteca.
    - Tela de Professor: Adaptada com saudação personalizada, foto de perfil, lista de disciplinas e banner de materiais para alunos.
    - Barra de navegação fixa na parte inferior, com itens clicáveis (ainda sem navegação funcional).

--- Project_React-College/App.js
+++ Project_React-College/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import EntryScreen from './src/screens/EntryScreen';
+import AlunoPage from './src/screens/AlunoPage';
+import ProfessorPage from './src/screens/ProfessorPage';
 
 const Stack = createStackNavigator();
-
 const AppNavigator = () => {
   return (
     <NavigationContainer>
@@ -15,9 +16,20 @@ const AppNavigator = () => {
           component={EntryScreen}
           options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name="Aluno"
+          component={AlunoPage}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="Professor"
+          component={ProfessorPage}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
 
 export default AppNavigator;
