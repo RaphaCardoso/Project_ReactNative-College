cfe78a75e218538293cc945f41b0005e072f194d
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-11-25 22:22:46 -0300

    alterando admin server

--- API_Project-College/src/config/database.js
+++ API_Project-College/src/config/database.js
@@ -5,10 +5,9 @@ const connectDB = async () => {
 
         const HOST_DB = process.env.HOST_DB;
 
-        const PASS_DB = process.env.DB_PASS;
+        const PASS_DB = process.env.PASS_DB;
 
-
-        const mongoURI = `mongodb+srv://${HOST_DB}:${PASS_DB}@database-college.fep21.mongodb.net/?retryWrites=true&w=majority&appName=Database-College`
+        const mongoURI = `mongodb+srv://${HOST_DB}:${PASS_DB}@college.9hqxc.mongodb.net/?retryWrites=true&w=majority&appName=college`
 
         await mongoose.connect(mongoURI);
 
